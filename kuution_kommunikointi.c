fc5b23282347a52b0decad3ac64a88d83d4001bb
  <>
2021-08-30 15:57:21 +0300

    Sekoituksen lukeminen toimii NxNxN-kuutioilla

--- kuution_kommunikointi.c
+++ kuution_kommunikointi.c
@@ -10,28 +10,52 @@ extern kuutio_t kuutio;
 extern kuva_t kuva;
 extern int viimeViesti;
 
+inline int __attribute__((always_inline)) kaistaksi(char c) {
+  int kaista = 1;
+  if('a' <= c && c <= 'z')
+    kaista = kuutio.N/2;
+  return kaista;
+}
+
+/*isoilla kuutioilla kaista merkitään numerolla*/
+inline int __attribute__((always_inline)) kaistaksi_iso(char c) {
+  return c - 0x30;
+}
+
+/*Jos kuution koko saavuttaa 20, kaista voi olla suurempi kuin yksinumeroinen luku
+  Tämä lukee kaistan ja kasvattaa indeksiä sopivan määrän*/
+int kaistaksi_valtava(char* str, int* id) {
+  int kaista;
+  if(sscanf(str + *id, "%i", &kaista) != 1)
+    kaista = 1;
+  do
+    ++*id;
+  while('0' <= str[*id] && str[*id] <= '9');
+  return kaista;
+}
+
 inline int __attribute__((always_inline)) puoleksi(char c) {
-  if('A' <= c && c <= 'Z')
-    c += 'a' - 'A';
+  if('a' <= c && c <= 'z')
+    c -= 'a' - 'A';
   switch(c) {
-  case 'r':
+  case 'R':
     return _r;
-  case 'l':
+  case 'L':
     return _l;
-  case 'u':
+  case 'U':
     return _u;
-  case 'd':
+  case 'D':
     return _d;
-  case 'b':
+  case 'B':
     return _b;
-  case 'f':
+  case 'F':
     return _f;
   default:
     return -1;
   }
 }
 
-inline char __attribute__((always_inline)) maaraksi(char c) {
+inline int __attribute__((always_inline)) maaraksi(char c) {
   switch(c) {
   case ' ':
     return 1;
@@ -44,9 +68,10 @@ inline char __attribute__((always_inline)) maaraksi(char c) {
   }
 }
 
+/*Ajanottosovellus laittaa sekoituksen jaettuun muistiin siirto kerrallaan.
+  Tämä funktio lukee siirrot jaetusta muistista ja kääntää kuutiota niitten mukaan*/
 void lue_siirrot(shmRak_s* ipc) {
-  int puoli=0;
-  char maara=0;
+  int puoli=0,kaista=0,maara=0;
   float aikaraja = 1.0;
   float nukkumaaika = 0.01e6; //µs
   float aika = 0;
@@ -63,12 +88,35 @@ void lue_siirrot(shmRak_s* ipc) {
     return;
   }
   int i=0;
-  while(ipc->data[i]) {
-    puoli = puoleksi(ipc->data[i++]);
-    maara = maaraksi(ipc->data[i++]);
-    siirto(puoli, 1, maara);
-    while(ipc->data[i] == ' ')
-      i++;
-  }
+  if(kuutio.N < 6)
+    while(ipc->data[i]) {
+      kaista = kaistaksi(ipc->data[i]);
+      puoli  = puoleksi(ipc->data[i++]);
+      maara  = maaraksi(ipc->data[i++]);
+      for(int j=1; j<=kaista; j++)
+	siirto(puoli, j, maara);
+      while(ipc->data[i] == ' ')
+	i++;
+    }
+  else if(kuutio.N < 20)
+    while(ipc->data[i]) {
+      kaista = kaistaksi_iso(ipc->data[i++]);
+      puoli  = puoleksi(ipc->data[i++]);
+      maara  = maaraksi(ipc->data[i++]);
+      for(int j=1; j<=kaista; j++)
+	siirto(puoli, j, maara);
+      while(ipc->data[i] == ' ')
+	i++;
+    }
+  else
+    while(ipc->data[i]) {
+      kaista = kaistaksi_valtava(ipc->data, &i);
+      puoli  = puoleksi(ipc->data[i++]);
+      maara  = maaraksi(ipc->data[i++]);
+      for(int j=1; j<=kaista; j++)
+	siirto(puoli, j, maara);
+      while(ipc->data[i] == ' ')
+	i++;
+    }
   kuva.paivita = 1;
 }
