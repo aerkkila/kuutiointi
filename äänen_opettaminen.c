6543f0238bc1f954a2eb81e07839c8fb61741cae
  <>
2022-02-03 03:00:55 +0200

    Kohdistin on liikkuva ja näppäinsidontoja on lisätty.

--- äänen_opettaminen.c
+++ äänen_opettaminen.c
@@ -13,6 +13,7 @@ void piirra_raidat();
 void aja();
 void aanen_opettaminen(float* data, int raitoja, int raidan_pit, snd_pcm_t* kahva);
 void skaalaa(float* data, int pit);
+uint64_t hetkinyt();
 #define ASETA_VARI(vari) SDL_SetRenderDrawColor(rend, vari.r, vari.g, vari.b, vari.a)
 #define DATAxKOHTA(raita,xkohta) ((raita)*raidan_pit + (xkohta)*ivali)
 
@@ -28,11 +29,13 @@ static SDL_Color piirtovari = {255,255,255,255};
 static SDL_Color kohdistin_paaraita = {255,80,0,255};
 static SDL_Color kohdistin_muuraita = {0,255,50,255};
 static struct {int x; int r;} kohdistin = {.x = 0, .r = 0};
+static int toiston_x;
 static float* data;
 static int raitoja, raidan_pit;
 static snd_pcm_t* kahva;
 static unsigned tuplaklikkaus_ms = 240;
 static int toistaa = 0;
+static uint64_t hiirihetki0, toistohetki0, hetki=0;
 
 void skaalaa(float* data, int pit) {
   float max = -INFINITY;
@@ -70,19 +73,24 @@ void piirra_raidat() {
   SDL_SetRenderTarget(rend, NULL);
 }
 
-void piirra_kohdistin() {
+int toiston_sijainti() {
+  return kohdistin.x + (hetkinyt()-toistohetki0) * 48 / ivali;
+}
+
+void piirra_kohdistin(int x, int r) {
   ASETA_VARI(kohdistin_muuraita);
-  SDL_RenderDrawLine(rend, kohdistin.x, 0, kohdistin.x, ikk_h);
+  SDL_RenderDrawLine(rend, x, 0, x, ikk_h);
   ASETA_VARI(kohdistin_paaraita);
-  SDL_RenderDrawLine(rend, kohdistin.x, kohdistin.r*raidan_kork, kohdistin.x, kohdistin.r*raidan_kork + raidan_h);
+  SDL_RenderDrawLine(rend, x, r*raidan_kork, x, r*raidan_kork + raidan_h);
 }
 
 void toista_kohdistin() {
+  snd_pcm_drop(kahva);
+  toistohetki0 = hetkinyt();
   while(snd_pcm_writei( kahva, data+DATAxKOHTA(kohdistin.r, kohdistin.x), raidan_pit-kohdistin.x*ivali ) < 0) {
     snd_pcm_prepare(kahva);
     fprintf(stderr, "Puskurin alitäyttö\n");
   }
-  fflush(stdout);
 }
 
 uint64_t hetkinyt() {
@@ -91,30 +99,83 @@ uint64_t hetkinyt() {
   return t.tv_sec*1000 + t.tv_usec/1000;
 }
 
+void kohdistin_sivulle(int maara) {
+  kohdistin.x += maara;
+  if(kohdistin.x < 0)
+    kohdistin.x = 0;
+  else if(kohdistin.x > ikk_w)
+    kohdistin.x = ikk_w;
+}
+
 void aja() {
   SDL_Event tapaht;
-  uint64_t hetki0=0, hetki=0;
+  int siirtoluku = 1;
+  unsigned vaihto = 0;
  ALKU:
   while(SDL_PollEvent(&tapaht)) {
     switch(tapaht.type) {
     case SDL_QUIT:
       return;
     case SDL_MOUSEBUTTONDOWN:
-      if( (hetki=hetkinyt()) - hetki0 < tuplaklikkaus_ms ) {
+      if( (hetki=hetkinyt()) - hiirihetki0 < tuplaklikkaus_ms ) {
 	toista_kohdistin();
 	break;
       }
-      hetki0 = hetki;
+      hiirihetki0 = hetki;
       kohdistin.x = tapaht.button.x;
       kohdistin.r = tapaht.button.y / raidan_kork;
       break;
     case SDL_KEYDOWN:
-      if( tapaht.key.keysym.sym == SDLK_SPACE ) {
+      switch(tapaht.key.keysym.scancode) {
+      case SDL_SCANCODE_H:
+	kohdistin_sivulle(-siirtoluku);
+	break;
+      case SDL_SCANCODE_L:
+	kohdistin_sivulle(siirtoluku);
+	break;
+      case SDL_SCANCODE_J:
+	if(vaihto)
+	  kohdistin.x = 0;
+	else
+	  kohdistin.r = (kohdistin.r+1) % raitoja;
+	break;
+      case SDL_SCANCODE_K:
+	kohdistin.r = (kohdistin.r-1+raitoja) % raitoja;
+	break;
+      case SDL_SCANCODE_SEMICOLON:
+	kohdistin.x = ikk_w;
+	break;
+      default:
+	break;
+      } //endswitch scancode
+      switch(tapaht.key.keysym.sym) {
+      case SDLK_SPACE:
 	if( (toistaa = (toistaa+1) % 2) )
 	  toista_kohdistin();
-	else 
+	else {
 	  snd_pcm_drop(kahva);
-      }
+	  if(vaihto)
+	    kohdistin.x = toiston_x;
+	}
+	break;
+      case SDLK_LSHIFT:
+	vaihto |= 1<<1;
+	break;
+      case SDLK_RSHIFT:
+	vaihto |= 1<<2;
+	break;
+      default:
+	if('1' <= tapaht.key.keysym.sym && tapaht.key.keysym.sym <= '9')
+	  siirtoluku = tapaht.key.keysym.sym - '0'; //H- tai L-näppäin siirtää kohdistinta painetun numeron verran
+      } //endswitch symcode
+      break; //keydown
+    case SDL_KEYUP:
+      if(tapaht.key.keysym.sym == SDLK_LSHIFT)
+	vaihto &= ~(1<<1);
+      else if (tapaht.key.keysym.sym == SDLK_RSHIFT)
+	vaihto &= ~(1<<2);
+      else if('1' <= tapaht.key.keysym.sym && tapaht.key.keysym.sym <= '9')
+	siirtoluku = 1;
       break;
     case SDL_WINDOWEVENT:
       if( tapaht.window.event != SDL_WINDOWEVENT_RESIZED)
@@ -126,7 +187,12 @@ void aja() {
     }
   }
   SDL_RenderCopy(rend, tausta, NULL, NULL);
-  piirra_kohdistin();
+  piirra_kohdistin(kohdistin.x, kohdistin.r);
+  if(toistaa) {
+    if( (toiston_x = toiston_sijainti()) > ikk_w )
+      toistaa = 0;
+    piirra_kohdistin(toiston_x, kohdistin.r);
+  }
   SDL_RenderPresent(rend);
   SDL_Delay(15);
   goto ALKU;
